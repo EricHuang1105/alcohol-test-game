9d8f00d5fa95fb17eee85e35c3076e7bd6bf3f57
unknown <931105eric@gmail.com>
2026-07-03 10:31:54 +0800

    調整小螢幕下滑箭頭的滑動幅度v6

--- src/App.vue
+++ src/App.vue
@@ -377,7 +377,7 @@ const scrollToBottom = () => {
   playClickSound();
   if (resultCardRef.value) {
     // 🌟 動態偵測：如果螢幕高度 <= 740px，只滑動 200px；大螢幕則維持 300px
-    const scrollDistance = window.innerHeight <= 740 ? 200 : 300;
+    const scrollDistance = window.innerHeight <= 740 ? 230 : 300;
 
     // 讓卡片向下捲動，並加上平滑過渡效果
     resultCardRef.value.scrollTo({
